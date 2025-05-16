2faf4eacb8a79aa07e65d56bb7ab9701a2b857ad
Tung Le <lenhattung@gmail.com>
2025-05-16 16:05:32 +0700

    ud
    ud

--- 2025.05.16/JavaApplication02/src/Ex04.java
+++ 2025.05.16/JavaApplication02/src/Ex04.java
@@ -8,5 +8,18 @@
  * @author tungi
  */
 public class Ex04 {
-    
+
+    public static void main(String[] args) {
+        int y = 5;
+        System.out.println("5>>1: " + (y >> 1));
+        
+        int x = -1;
+        System.out.println("-1<<1: " + (x << 1)); //-2
+        System.out.println("-1>>1: " + (x >> 1)); //-1
+        System.out.println("-1>>>1: " + (x >>> 1)); ///....
+        
+        System.out.println(10&7);
+        System.out.println(10|7);
+        System.out.println(10^7);
+    }
 }
---
+++ 2025.05.16/JavaApplication02/src/Ex05.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Ex05 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+        double z = (double)x/y;
+        System.out.println(z);
+                
+        
+        int a = 5;
+        a/=2;
+        System.out.println(a);///
+    }
+}
