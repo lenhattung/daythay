66553b0da9a57b4a319199d8e881f71cf0a03c3c
Tung Le <lenhattung@gmail.com>
2025-05-16 16:40:56 +0700

    ud
    ud

---
+++ 2025.05.16/JavaApplication02/src/Ex08.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Ex08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a integer number: ");
+        int a = sc.nextInt();
+        String result = "a = " + a;
+        System.out.println(result);
+    }
+}
---
+++ 2025.05.16/JavaApplication02/src/Ex09.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Ex09 {
+    public static void main(String[] args) {
+        // Giai phuong trinh bac 2
+        // ax^2 + bx + c = 0;
+        // input a , b, c
+        // out: result
+
+    }
+}
