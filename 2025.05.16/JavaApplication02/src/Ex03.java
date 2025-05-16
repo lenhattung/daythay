2bbf6d46d62c9c932099bd91bbdd9410571192b9
Tung Le <lenhattung@gmail.com>
2025-05-16 15:46:22 +0700

    ud
    ud

---
+++ 2025.05.16/JavaApplication02/src/Ex03.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Ex03 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        
+        System.out.println(++a);
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(--a);
+        
+        System.out.println(++a + b++);
+    }
+}
