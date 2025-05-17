987dcf1f20d2e96644a39aac8173500ca53f3490
Tung Le <lenhattung@gmail.com>
2025-05-17 16:04:14 +0700

    ud
    ud

---
+++ 2025.05.17/Stack_02/src/MyStack.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class MyStack {
+    Node top;
+
+    public MyStack() {
+        top = null;
+    }
+    
+    public boolean isEmpty(){
+        
+    }
+    
+    public void push(Book b){
+        
+    }
+    
+    public Book pop(){
+        
+    }
+    
+    public Book peek(){
+        
+    }
+    
+    public void display(){
+        
+    }
+}
